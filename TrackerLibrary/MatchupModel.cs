cc38a969d3609b9128e655127020678cd76e5dc3
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-09 07:21:28 +0300

    Added basic models implementation

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary;
+
+public class MatchupModel
+{
+    public List<MatchupEntryModel> Entries { get; set; } = new();
+    public TeamModel Winner { get; set; }
+    public int MatchupRound { get; set; }
+}
---
+++ TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary;
+
+public class TournamentModel
+{
+    public string TournamentName { get; set; }
+    public decimal EntryFee { get; set; }
+    public List<TeamModel> EnteredTeams { get; set; } = new();
+    public List<PrizeModel> Prizes { get; set; } = new();
+    public List<List<MatchupModel>> Rounds { get; set; } = new();
+}
