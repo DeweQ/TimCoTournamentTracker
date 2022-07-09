04d89a47f344d542034e3234b4f076f7fc2d6cee
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-09 07:10:21 +0300

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary;
+
+public class TeamModel
+{
+    public List<Person> TeamMembers { get; set; } = new();
+    public string TeamName { get; set; }
+}
