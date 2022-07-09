2bf9dc3ead5a5bdfb3bfc1faadcf72193bcb463f
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-09 08:28:19 +0300

    Added WinForms UI project and TournamentViewerForm in it

---
+++ TrackerUI/TournamentViewerForm.cs
@@ -0,0 +1,15 @@
+namespace TrackerUI
+{
+    public partial class TournamentViewerForm : Form
+    {
+        public TournamentViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
