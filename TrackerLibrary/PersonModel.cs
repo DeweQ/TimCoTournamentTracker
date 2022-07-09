6b9fd75a563a9a437ca18b1e5e41170146723fce
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-09 07:12:40 +0300

    Added PersonModel

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary;
+
+public class PersonModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAdress { get; set; }
+    public string CellphoneNumber { get; set; }
+}
--- TrackerLibrary/TeamModel.cs
+++ TrackerLibrary/TeamModel.cs
@@ -8,6 +8,6 @@ namespace TrackerLibrary;
 
 public class TeamModel
 {
-    public List<Person> TeamMembers { get; set; } = new();
+    public List<PersonModel> TeamMembers { get; set; } = new();
     public string TeamName { get; set; }
 }
