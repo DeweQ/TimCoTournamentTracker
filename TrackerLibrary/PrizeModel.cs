2014e6c4142776c93b8c9106353adc197bfd8873
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-10 09:13:50 +0300

    added id property

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -6,24 +6,29 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary;
 /// <summary>
-/// Represents the prize for a tournament.
+/// Represents what the prize is for a given place.
 /// </summary>
 public class PrizeModel
 {
     /// <summary>
-    /// Represents what place this prize for.
+    /// The unique identifier for the prize.
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// The numeric identifier for the place (2 for second place, etc.)
     /// </summary>
     public int PlaceNumber { get; set; }
     /// <summary>
-    /// Represents a custom name for this place.
+    /// The friendly name for the place (second place, first runner up, etc.)
     /// </summary>
     public string PlaceName { get; set; }
     /// <summary>
-    /// Represents the amount of prize for this place.
+    /// The fixed amount this place earns or zero if it is not used.
     /// </summary>
     public decimal PrizeAmount { get; set; }
     /// <summary>
-    /// Represents the persentage of prize for this place.
+    /// The number that represents the percentage of the overall take or zero 
+    /// if it is not used. The percentage is a fraction of 1 (so 0.5 for 50%).
     /// </summary>
     public double PrizePresentage { get; set; }
 }
