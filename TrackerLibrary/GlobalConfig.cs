40c1af806f14a1e1e0f58162c07012d389bfb43b
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-15 15:20:24 +0300

    Changed source of appsettings to be a json file

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 using System.Configuration;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,10 @@ public static class GlobalConfig
     public const string MatchupsFile = "MatchupModels.csv";
     public const string MatchupEntriesFile = "MatchupEntryModels.csv";
 
+    private static IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+    public static Settings Settings = config.GetRequiredSection("Settings").Get<Settings>();
+
     public static IDataConnection Connection { get; private set; }
 
     public static void InitializeConnections(DatabaseType db)
@@ -24,18 +29,26 @@ public static class GlobalConfig
         if (db == DatabaseType.Sql)
         {
             SqlConnector sql = new();
-            Connection=sql;
+            Connection = sql;
         }
 
-        else if(db == DatabaseType.TextFile)
+        else if (db == DatabaseType.TextFile)
         {
             TextConnector text = new();
-            Connection=text;
+            Connection = text;
         }
     }
 
     public static string CnnString(string name)
     {
-        return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        return Settings.ConnectionStrings[name];
+        //return System.Configuration.ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
     }
+
+    //public static string AppKeyLookup(string key)
+    //{
+    //    return System.Configuration.ConfigurationManager.AppSettings[key];
+
+    //}
 }
